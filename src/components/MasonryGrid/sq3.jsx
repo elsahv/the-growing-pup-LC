5c4298372cd0375484032fbb2ec7a5d75c096565
elsa <elsahvy@gmail.com>
2026-03-29 22:52:21 -0700

    play around content

--- src/components/MasonryGrid/sq3.jsx
+++ src/components/MasonryGrid/sq3.jsx
@@ -1,8 +1,8 @@
 const Sq3 = () => {
   return (
-    <div id="sq3" className="bg- mt-[110px] md:ml-5 rounded-lg">
-      <div className="text-test2  bg-teal sq-styles md:px-3 px-0">
-        <ul className="md:pl-3 pl-2 pt-3 pb-3 pr-1 xl:text-xl text-sm md:space-y-3 space-y-1 ">
+    <div id="sq3" className=" mt-[110px] md:ml-5 rounded-lg">
+      <div className="bg-carribean text-test2  bg-teal sq-styles md:px-3 px-0">
+        <ul className="md:pl-3 pl-2 pt-3 pb-3 pr-1 xl:text-xl text-sm md:space-y-3">
           <li>web design</li>
           <li>software development</li>
           <li>copywriting</li>
